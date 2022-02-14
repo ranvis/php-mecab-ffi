52e369581a40631783545ad85bcccc1438335ef2
SATO Kentaro <kentaro@ranvis.com>
2022-02-14 23:54:55 +0900

    Support OPcache preloading.

--- src/Env.php
+++ src/Env.php
@@ -17,6 +17,15 @@ class Env
     private string $libPath;
     private FFI $lib;
 
+    /**
+     * Instantiate MeCab environment.
+     *
+     * Load and initialize MeCab shared library.
+     *
+     * @param string|null|false $libPath A path to the library.
+     * Null if it can be found on the default location.
+     * False is for internal use.
+     */
     public function __construct(
         string|null|false $libPath = null,
     ) {
@@ -24,10 +33,46 @@ class Env
             $libPath ??= 'libmecab.' . PHP_SHLIB_SUFFIX;
             $defs = Header::get();
             $this->lib = FFI::cdef($defs, $libPath);
+            $this->libPath = $libPath;
         }
-        $this->libPath = $libPath;
     }
 
+    /**
+     * Preload MeCab library.
+     *
+     * This method is expected to be called from opcache.preload script.
+     *
+     * @param string|null $libPath A path to the library.
+     * Null if it can be found on the default location.
+     * @param string|null $scope A scope name to preload to.
+     * Null to load to the default scope.
+     * @return self
+     */
+    public static function preload(?string $libPath = null, ?string $scope = null): ?self
+    {
+        $instance = new static(false);
+        $instance->libPath = $libPath ?? 'libmecab.' . PHP_SHLIB_SUFFIX;
+        $preloader = $instance->getPreloader($scope);
+        if (($tmpPath = tempnam(sys_get_temp_dir(), 'tmpMeCab')) === false) {
+            throw new \RuntimeException("Unable to create a temporary file.");
+        }
+        try {
+            if (!file_put_contents($tmpPath, $preloader)) {
+                throw new \RuntimeException("Unable to write to the temporary file.");
+            }
+            $instance->lib = FFI::load($tmpPath);
+        } finally {
+            unlink($tmpPath);
+        }
+        return $instance;
+    }
+
+    /**
+     * Instantiate MeCab environment from preloaded state.
+     *
+     * @param string|null $scope Null, or a non-default scope name set for preloading.
+     * @return static A created instance.
+     */
     public static function fromScope(?string $scope = null): static
     {
         $scope ??= self::SCOPE_NAME;
@@ -36,6 +81,7 @@ class Env
         return $instance;
     }
 
+    /** @internal */
     public function lib(): FFI
     {
         return $this->lib;
@@ -61,6 +107,7 @@ class Env
         return new Lattice($this);
     }
 
+    /** @internal */
     public function getPreloader(?string $scope = null): string
     {
         $scope ??= self::SCOPE_NAME;
